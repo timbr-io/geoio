900a6b16f8ba4b47a374a231b7a92402dc2ce92b
Chris Helm <christopher.helm@gmail.com>
2017-01-27 21:11:14 -0700

    adding a rough cut at ipe image

--- geoio/__init__.py
+++ geoio/__init__.py
@@ -4,7 +4,6 @@ import logging as _logging
 # Pull in main modules/classes
 from base import GeoImage
 from dg import DGImage
-from ipe.image import Image
 import constants
 import utils
 #import downsample  # numba compile is trigger at import and it necessarily slow
